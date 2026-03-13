0472069bb30fe85d2a8cf6f63d58ddfeeed3517c
SudhanvSK <spandansudhanv@gmail.com>
2026-03-13 21:10:42 +0530

    Time: 63 ms (20.16%), Space: 93 MB (13.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        //Submitted again for 75DaysLeetCodeChallenge DAY 2
         unordered_map<int,int> c;
         for(auto i : nums) c[i]++;
         for(auto &i : c) if(i.second>=2) return true;
         return false;
     }
 };
+ 
