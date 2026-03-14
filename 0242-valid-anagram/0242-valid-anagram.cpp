326c65d463820e00eeee10fdb6232e9c22cd1874
SudhanvSK <spandansudhanv@gmail.com>
2026-03-14 20:01:51 +0530

    Time: 9 ms (7.28%), Space: 9.6 MB (91.29%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //Submitted again for 75DaysLeetCodeChallenge DAY 3
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if (s==t) return true;
+        return false;
+    }
+};
