0fb320e17301ff1aec2ebb226df5af69f18daa61
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-03-31 20:39:59 +0530

    Time: 4 ms (44.55%), Space: 23.6 MB (21.69%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,36 +1,25 @@
 class MinStack {
-    // Submitted for 75DaysLeetCodeChallenge DAY 20
+     // Submitted for 75DaysLeetCodeChallenge DAY 20
 public:
-    stack <int> st;
-    multiset <int> s;
-    MinStack() {
-        
-    }
-    
+    stack<int> st, mini;
+
+    MinStack() {}
+
     void push(int val) {
-        s.insert(val);
         st.push(val);
+        if (mini.empty() || val <= mini.top()) mini.push(val);
     }
-    
+
     void pop() {
-        s.erase(s.find(st.top()));
+        if (st.top() == mini.top()) mini.pop();
         st.pop();
     }
-    
+
     int top() {
         return st.top();
     }
-    
+
     int getMin() {
-        return *s.begin();
+        return mini.top();
     }
 };
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
