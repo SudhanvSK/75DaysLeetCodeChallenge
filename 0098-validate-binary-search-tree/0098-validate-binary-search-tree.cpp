89ff57006945443bce70bc5ad5656cfb73add8ff
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-04-22 20:56:34 +0530

    Time: 0 ms (100%), Space: 21.8 MB (94.85%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    // Submitted for 75DaysLeetCodeChallenge DAY 42
+
+    bool check(TreeNode* root, long long low, long long high) {
+        if (root == NULL) return true;
+        if (root->val <= low || root->val >= high) return false;
+        return check(root->left, low, root->val) && check(root->right, root->val, high);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return check(root, LLONG_MIN, LLONG_MAX);
+    }
+};
