e76bb2f1b39515fd1c722ea3c2710c79c0eb6817
Sudhanv Sanjeev Kulkarni <spandansudhanv@gmail.com>
2026-04-24 22:38:40 +0530

    Time: 31 ms (24.64%), Space: 50.5 MB (75.88%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,53 @@
+class Trie {
+// Submitted for 75DaysLeetCodeChallenge DAY 44
+
+private:
+    struct Node {
+        Node* children[26];
+        bool isEnd;
+
+        Node() {
+            isEnd = false;
+            for (int i = 0; i < 26; i++) {
+                children[i] = nullptr;
+            }
+        }
+    };
+
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node* curr = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (curr->children[idx] == nullptr) curr->children[idx] = new Node();
+            curr = curr->children[idx];
+        }
+        curr->isEnd = true;
+    }
+    
+    bool search(string word) {
+        Node* curr = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (curr->children[idx] == nullptr) return false;
+            curr = curr->children[idx];
+        }
+        return curr->isEnd;
+    }
+    
+    bool startsWith(string prefix) {
+        Node* curr = root;
+        for (char c : prefix) {
+            int idx = c - 'a';
+            if (curr->children[idx] == nullptr) return false;
+            curr = curr->children[idx];
+        }
+        return true;
+    }
+};
